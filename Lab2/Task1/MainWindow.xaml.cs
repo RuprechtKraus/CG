e68ea4a4447b9ac19304e3ebf9bfacc66e744c6f
Ruprecht <43097128+RuprechtKraus@users.noreply.github.com>
2023-03-10 19:36:29 +0300

    Добавлена загрузка изображений

--- Lab2/Task1/MainWindow.xaml.cs
+++ Lab2/Task1/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace Task1
 {
@@ -23,6 +24,18 @@ namespace Task1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Open_Click( object sender, RoutedEventArgs e )
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.jpg;*,png)|*.jpg;*.png";
+
+            if ( dialog.ShowDialog() == true )
+            {
+                string filePath = dialog.FileName;
+                ImageView.Source = new BitmapImage( new Uri( filePath ) );
+            }
         }
     }
 }
