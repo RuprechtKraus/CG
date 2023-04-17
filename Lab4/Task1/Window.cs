a8be29b0bd65c94ac1b02669616848038cd94f6e
Ruprecht <43097128+RuprechtKraus@users.noreply.github.com>
2023-04-17 21:47:01 +0300

    Движение и вращение камеры

--- Lab4/Task1/Window.cs
+++ Lab4/Task1/Window.cs
@@ -5,6 +5,7 @@ using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using Task1.Objects;
 using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace Task1
 {
@@ -15,11 +16,15 @@ namespace Task1
         private const float ZFar = 10.0f;
         private const float FrustumSize = 2.0f;
         private const float DistanceToOrigin = 2.0f;
+        private const float CameraSpeed = 0.01f;
 
         public float AspectRatio => (float) Width / Height;
 
         private Cube _cube = new Cube( 0.5f );
-        private bool _leftButtonPressed = false;
+
+        private Vector3 position = new Vector3( 0.0f, 0.0f, 5.0f );
+        private Vector3 front = new Vector3( 0.0f, 0.0f, -5.0f );
+        private Vector3 up = new Vector3( 0.0f, 1.0f, 0.0f );
 
         public Window( int width, int height, string title )
             : base( width, height, GraphicsMode.Default, title )
@@ -32,16 +37,89 @@ namespace Task1
             _cube.SetSideColor( CubeSide.POSITIVE_Z, 1.0f, 0.0f, 1.0f, 1.0f );
         }
 
-        private bool _left = true;
-
         protected override void OnLoad( EventArgs e )
         {
             base.OnLoad( e );
+
             GL.ClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
             GL.Enable( EnableCap.CullFace );
             GL.CullFace( CullFaceMode.Back );
             GL.FrontFace( FrontFaceDirection.Ccw );
             GL.Enable( EnableCap.DepthTest );
+
+            Matrix4 view = Matrix4.LookAt( position, position + front, up );
+            GL.LoadMatrix( ref view );
+
+            MouseMove += Window_MouseMove;
+            KeyDown += Window_KeyDown;
+        }
+
+        private void Window_KeyDown( object sender, KeyboardKeyEventArgs e )
+        {
+            if ( !Focused )
+            {
+                return;
+            }
+
+            switch ( e.Key )
+            {
+                case Key.W:
+                    position += front * 0.01f;
+                    break;
+                case Key.S:
+                    position -= front * 0.01f;
+                    break;
+                case Key.A:
+                    position -= Vector3.Normalize( Vector3.Cross( front, up ) ) * 0.02f;
+                    break;
+                case Key.D:
+                    position += Vector3.Normalize( Vector3.Cross( front, up ) ) * 0.02f;
+                    break;
+                case Key.Space:
+                    position += up * 0.01f;
+                    break;
+                case Key.ShiftLeft:
+                    position -= up * 0.01f;
+                    break;
+                default:
+                    return;
+            }
+
+            //Matrix4 view = Matrix4.LookAt( position, position + front, up );
+            //GL.LoadMatrix( ref view );
+        }
+
+        private float _yaw = -90;
+        private float _pitch = 0;
+
+        private void Window_MouseMove( object sender, MouseMoveEventArgs e )
+        {
+            if ( e.Mouse.LeftButton == ButtonState.Pressed )
+            {
+                //float rotateX = (float) e.YDelta * 180 / Height;
+                //float rotateY = (float) e.XDelta * 180 / Width;
+
+                //RotateCamera( rotateX, rotateY, e.XDelta, e.YDelta );
+
+                if ( _pitch > 89.0f )
+                {
+                    _pitch = 89.0f;
+                }
+                else if ( _pitch < -89.0f )
+                {
+                    _pitch = -89.0f;
+                }
+                else
+                {
+                    _yaw += e.XDelta * 0.2f;
+                    _pitch -= e.YDelta * 0.2f;
+                }
+
+                front.X = (float) Math.Cos( MathHelper.DegreesToRadians( _pitch ) ) * (float) Math.Cos( MathHelper.DegreesToRadians( _yaw ) );
+                front.Y = (float) Math.Sin( MathHelper.DegreesToRadians( _pitch ) );
+                front.Z = (float) Math.Cos( MathHelper.DegreesToRadians( _pitch ) ) * (float) Math.Sin( MathHelper.DegreesToRadians( _yaw ) );
+                front.Normalize();
+            }
         }
 
         protected override void OnUpdateFrame( FrameEventArgs e )
@@ -96,15 +174,63 @@ namespace Task1
             //GL.Color3( 0.0f, 0.0f, 1.0f );
             //GL.Rect( -0.5f, -0.5f, 0.5f, 0.5f );
 
-            var view = Matrix4.LookAt(
-                new Vector3( 2.0f, 2.0f, 2.0f ),
-                new Vector3( 0.0f, 0.0f, 0.0f ),
-                new Vector3( 0.0f, 1.0f, 0.0f ) );
+            //Matrix4 view = Matrix4.LookAt( position, position + front, up );
+
+            Matrix4 view = Matrix4.LookAt( position, position + front, up );
             GL.LoadMatrix( ref view );
 
             _cube.Draw();
 
             SwapBuffers();
+        }
+
+        private void RotateCamera( float rotateX, float rotateY, float dx, float dy )
+        {
+            GL.GetFloat( GetPName.ModelviewMatrix, out Matrix4 modelView );
+
+            Vector3 xAxis = new Vector3( modelView.M11, modelView.M21, modelView.M31 );
+            Vector3 yAxis = new Vector3( modelView.M12, modelView.M22, modelView.M32 );
+
+            front.X += dx / Width * 3;
+            front.Y -= dy / Height * 3;
+            //Matrix4 view = Matrix4.LookAt( position, position + front, up );
+            //GL.LoadMatrix( ref view );
+
+            //GL.Rotate( rotateX, xAxis.X, xAxis.Y, xAxis.Z );
+            //GL.Rotate( rotateY, yAxis.X, yAxis.Y, yAxis.Z );
+
+            //NormalizeModelViewMatrix();
+        }
+
+        private void NormalizeModelViewMatrix()
+        {
+            GL.GetFloat( GetPName.ModelviewMatrix, out Matrix4 modelView );
+
+            Vector3 xAxis = new Vector3( modelView.M11, modelView.M21, modelView.M31 );
+            xAxis.Normalize();
+            Vector3 yAxis = new Vector3( modelView.M12, modelView.M22, modelView.M32 );
+            yAxis.Normalize();
+
+            Vector3 zAxis = Vector3.Cross( xAxis, yAxis );
+            zAxis.Normalize();
+            xAxis = Vector3.Cross( yAxis, zAxis );
+            xAxis.Normalize();
+            yAxis = Vector3.Cross( zAxis, xAxis );
+            yAxis.Normalize();
+
+            modelView.M11 = xAxis.X;
+            modelView.M21 = xAxis.Y;
+            modelView.M31 = xAxis.Z;
+            modelView.M12 = yAxis.X;
+            modelView.M22 = yAxis.Y;
+            modelView.M23 = yAxis.Z;
+            modelView.M13 = zAxis.X;
+            modelView.M23 = zAxis.Y;
+            modelView.M33 = zAxis.Z;
+
+            //var view = Matrix4.LookAt( _camera.Row0, _camera.Row1, _camera.Row2 );
+            //modelView = modelView * view;
+            GL.LoadMatrix( ref modelView );
         }
 
         private void SetupProjectionMatrix()
