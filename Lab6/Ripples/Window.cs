96fe8dc6c6ff38b5248b79399bc4fa6aacffa178
Ruprecht <43097128+RuprechtKraus@users.noreply.github.com>
2023-06-14 22:10:11 +0300

    Создал шейдеры

--- Lab6/Ripples/Window.cs
+++ Lab6/Ripples/Window.cs
@@ -2,11 +2,14 @@
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using Toolkit.Shaders;
 
 namespace Ripples;
 
 public class Window : GameWindow
 {
+    private ShaderProgram _program = null!;
+    
     public Window( int width, int height, string title )
         : base( GameWindowSettings.Default, new NativeWindowSettings
         {
@@ -21,6 +24,37 @@ public class Window : GameWindow
         base.OnLoad();
 
         GL.ClearColor( Color.White );
+
+        InitializeShaders();
+    }
+    
+    private void InitializeShaders()
+    {
+        Shader vertexShader = ShaderLoader.LoadShader(
+            ShaderType.VertexShader,
+            @"../../../Shaders/ripples.vert" );
+        Shader fragmentShader = ShaderLoader.LoadShader(
+            ShaderType.FragmentShader,
+            @"../../../Shaders/ripples.frag" );
+
+        ShaderCompiler shaderCompiler = new();
+        shaderCompiler.CompileShader( vertexShader );
+        shaderCompiler.CompileShader( fragmentShader );
+        shaderCompiler.CheckStatus();
+
+        _program = new ShaderProgram();
+        _program.AttachShader( vertexShader );
+        _program.AttachShader( fragmentShader );
+
+        ShaderProgramLinker linker = new();
+        linker.LinkProgram( _program );
+        linker.CheckStatus();
+
+        _program.DetachShader( vertexShader );
+        _program.DetachShader( fragmentShader );
+
+        vertexShader.Delete();
+        fragmentShader.Delete();
     }
 
     protected override void OnRenderFrame( FrameEventArgs args )
