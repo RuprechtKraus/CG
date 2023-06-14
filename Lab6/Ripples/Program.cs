a633d773f816cad3ec28e0a4c211e9d107936440
Ruprecht <43097128+RuprechtKraus@users.noreply.github.com>
2023-06-14 21:18:09 +0300

    Создал проект Ripples

---
+++ Lab6/Ripples/Program.cs
@@ -0,0 +1,4 @@
+﻿using Ripples;
+
+using var window = new Window( 800, 800, "Ripples" );
+window.Run();
---
+++ Lab6/Ripples/Window.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace Ripples;
+
+public class Window : GameWindow
+{
+    public Window( int width, int height, string title )
+        : base( GameWindowSettings.Default, new NativeWindowSettings
+        {
+            Size = ( width, height ),
+            Title = title
+        } )
+    {
+    }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+
+        GL.ClearColor( Color.White );
+    }
+
+    protected override void OnRenderFrame( FrameEventArgs args )
+    {
+        base.OnRenderFrame( args );
+    }
+
+    protected override void OnResize( ResizeEventArgs e )
+    {
+        base.OnResize( e );
+        
+        GL.Viewport( 0, 0, e.Width, e.Height );
+    }
+}
